5697c526f47e554aa29a77bdbc1c382f657efdb7
Dmitrii Kim <mitya_kim@icloud.com>
2022-08-22 23:37:44 -0700

    feat: add RewardsButton

--- Starbucks/RewardTileView.swift
+++ Starbucks/RewardTileView.swift
@@ -11,6 +11,10 @@ import UIKit
 class RewardTileView: UIView {
     
     let balanceView = BallanceView()
+    var rewardsButton = UIButton()
+    let rewardsGraphView = UIView()
+    let starRewardsView = UIView()
+    var detailsButton = UIButton()
     
     override init(frame: CGRect) {
         super.init(frame: .zero)
@@ -31,14 +35,69 @@ class RewardTileView: UIView {
 extension RewardTileView {
     func style() {
         balanceView.translatesAutoresizingMaskIntoConstraints = false
+        rewardsGraphView.translatesAutoresizingMaskIntoConstraints = false
+        starRewardsView.translatesAutoresizingMaskIntoConstraints = false
+        
+        makeRewardsButton()
+        
+        detailsButton.translatesAutoresizingMaskIntoConstraints = false
+        detailsButton.titleLabel?.text = "Details"
+        detailsButton.backgroundColor = .systemBlue
+        
+        
+    }
+    
+    func makeRewardsButton() {
+        rewardsButton.translatesAutoresizingMaskIntoConstraints = false
+//        rewardsButton.addTarget(self, action: #selector(rewardButtonTapped), for: .primaryActionTriggered)
+        
+        let configuration = UIImage.SymbolConfiguration(scale: .small)
+        let image = UIImage(systemName: "chevron.down", withConfiguration: configuration)
+        
+        rewardsButton.setImage(image, for: .normal)
+        rewardsButton.imageView?.tintColor = .label
+        rewardsButton.imageView?.contentMode = .scaleAspectFit
+        
+        rewardsButton.setTitle("Rewards options", for: .normal)
+        rewardsButton.setTitleColor(.label, for: .normal)
+        rewardsButton.titleLabel?.font = UIFont.preferredFont(forTextStyle: .footnote)
+        
+        rewardsButton.semanticContentAttribute = .forceRightToLeft
+        rewardsButton.imageEdgeInsets = UIEdgeInsets(top: 2, left: 20, bottom: 0, right: 0)
+        rewardsButton.contentEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 20)
     }
     
     func layout() {
         addSubview(balanceView)
+        addSubview(rewardsButton)
+        addSubview(rewardsGraphView)
+        addSubview(starRewardsView)
+        addSubview(detailsButton)
         
         NSLayoutConstraint.activate([
+            // balanceView
             balanceView.topAnchor.constraint(equalTo: topAnchor),
-            balanceView.leadingAnchor.constraint(equalToSystemSpacingAfter: leadingAnchor, multiplier: 2)
+            balanceView.leadingAnchor.constraint(equalToSystemSpacingAfter: leadingAnchor, multiplier: 2),
+            
+            // rewardsButton
+            rewardsButton.centerYAnchor.constraint(equalTo: balanceView.pointsLabel.centerYAnchor),
+            trailingAnchor.constraint(equalToSystemSpacingAfter: rewardsButton.trailingAnchor, multiplier: 3),
+            
+            // rewardsGraphView
+            rewardsGraphView.topAnchor.constraint(equalToSystemSpacingBelow: balanceView.bottomAnchor, multiplier: 1),
+            rewardsGraphView.centerXAnchor.constraint(equalTo: centerXAnchor),
+            rewardsGraphView.widthAnchor.constraint(equalToConstant: frame.width),
+            
+            // starRewardsView
+            starRewardsView.topAnchor.constraint(equalTo: rewardsGraphView.bottomAnchor, constant: 8),
+            starRewardsView.leadingAnchor.constraint(equalToSystemSpacingAfter: leadingAnchor, multiplier: 1),
+            trailingAnchor.constraint(equalToSystemSpacingAfter: starRewardsView.trailingAnchor, multiplier: 1),
+            
+            // detailsButton
+            detailsButton.topAnchor.constraint(equalToSystemSpacingBelow: starRewardsView.bottomAnchor, multiplier: 2),
+            detailsButton.leadingAnchor.constraint(equalTo: balanceView.leadingAnchor),
+            bottomAnchor.constraint(equalToSystemSpacingBelow: detailsButton.bottomAnchor, multiplier: 2)
+
         ])
     }
 }
